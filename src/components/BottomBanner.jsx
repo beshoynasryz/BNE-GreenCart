3c4a63bdfd507e4e10203612b81e4443e82bfda6
Beshoy Nasry <beshoy.nasry@tradesocio.com>
2025-04-23 15:05:20 +0200

    news letter section

--- src/components/BottomBanner.jsx
+++ src/components/BottomBanner.jsx
@@ -15,7 +15,7 @@ const BottomBanner = () => {
         alt="Bottom Banner Small"
         className="w-full h-auto object-cover block sm:hidden"
       />
-      <div className="absolute inset-0 flex flex-col item-center md:items-end md:justify-center pt-16 md:pt-0 md:pr-24">
+      <div className="absolute inset-0 flex flex-col  item-center md:items-end md:justify-center pt-16 md:pt-0 md:pr-24 ">
         <div>
           <h1 className="text-2xl md:text-3xl font-semibold mb-6 text-primary ">
             Why We Are The Best ?{" "}
@@ -27,8 +27,10 @@ const BottomBanner = () => {
                 alt="Feature Icon"
                 className="w-10 h-10 object-contain"
               />
-              <p className="text-base font-semibold">{feature.title}</p>
-              <p className="text-base">{feature.description}</p>
+             <div>
+             <p className="text-base font-semibold">{feature.title}</p>
+             <p className="text-base">{feature.description}</p>
+             </div>
             </div>
           ))}
         </div>
